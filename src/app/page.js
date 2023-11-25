55740bc54e7a201c158d7a7e3efd7eb3b63f0fb3
maaaz5 <mohamade.amine12345@gmail.com>
2023-11-26 00:00:25 +0100

    description section added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 
 import Intro from "../components/Intro";
+import Description from "../components/Description";
 
 export default function Home() {
   useEffect(() => {
@@ -14,6 +15,7 @@ export default function Home() {
   return (
     <main>
       <Intro />
+      <Description />
     </main>
   );
 }
